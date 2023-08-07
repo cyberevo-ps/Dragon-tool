0f1dcd9cd0fee96f89ca80594fadaff8957c877d
bo_haydar <139859144+bohaydar@users.noreply.github.com>
2023-08-07 12:47:33 +0300

    Add files via upload

---
+++ Dragon-Tool.py
@@ -0,0 +1,114 @@
+import os
+import sys,time
+os.system("clear")
+text = """
+                                   ;x. ,d.
+                                   ;Kl.'0l
+                    ...  ,Kko;;Kd;. 'KXo;00:
+                    .xNXK0XNNNNNNNXOdxNNKxkNO,
+                .ldxOKNNNNNNNNNNNNNNNNNNNNXXNNk;
+                 oNNNNNNNXOxdddddk0XNNNNNNNNNNNN0l.
+             .:xKNNNNNKo'          .,lkXNNNNNNNNNNXd.
+              'dXNNNNk.                .:ONNNNNNNX0NK;
+              oXNNNN0                     ,0NNNNNXdxKXc
+            .dOKNNNNo                       oXNNNNNXKNNd.
+              .0NNNNO                        .dXNNNNNNNK'
+              cNNNNNNd                          ;d0NNNNNx.
+              ;;'0NNNNO'                           'dXNNNXo.
+                 :NXNNNXx,                           .kNNNNNK,
+                  :.,xXNNNKo,                          kNNNXo
+                      .:xKNNNKd:.                      lN0l.
+                          ':xKNNN0d:.                  '.
+                              .:oOXNNKxc.
+                                  .'ckKNNO:
+                                       .cONKl
+                                          .oX0'
+                                            .xK,
+                                              cK.
+                                               x,
+                                               ..
+        ____                              _____         _
+       |    \ ___ ___ ___ ___ ___   ___  |_   _|___ ___| |
+       |  |  |  _| .'| . | . |   | |___|   | | | . | . | |
+       |____/|_| |__,|_  |___|_|_|         |_| |___|___|_|
+                     |___|
+
+            The Dragon Tool For Noob Hackers '_' :)
+                 By Bo Haydar El Mo3alem
+
+  ________________________________________________________________
+ |Youtube:https://youtube.com/@BO_HAYDAR_HK  without mistakes √   |
+ |Tele:https://t.me/txrtm                    Easy for Noobs √     |
+ |Tele Team:https://t.me/+EHo65POl1yA2ZjA0   All Hacking Tools √  |
+ |________________________________________________________________|
+
+              Name Tool:Dragon-Tool     Version: 1.4
+              by:BO HAYDAR              Team: ARB TXR TEAM
+
+       _____________________________________________________
+      |_____________________________________________________|
+      |                                                     |
+      |           [1]      ==>     Android Virus            |
+      |           [2]      ==>     Google Cards             |
+      |           [3]      ==>     Hacking Tools            |
+      |           [4]      ==>     Masked Links             |
+      |           [5]      ==>     DDoS Attack              |
+      |           [6]      ==>     Metasploite Fix          |
+      |           [7]      ==>     Peshing Website          |
+      |           [8]      ==>     Generator Payload        |
+      |           [9]      ==>     Ip Info                  |
+      |           [10]     ==>     Ip website               |
+      |           [11]     ==>     Install YouTube video    |
+      |           [12]     ==>     Scan Website & IP        |
+      |           [13]     ==>     Cam Hackers              |
+      |           [14]     ==>     Ngrok Install            |
+      |           [15]     ==>     Ngrok Ports              |
+      |           [16]     ==>     Instgram Info            |
+      |           [17]     ==>     Termux Style             |
+      |_____________________________________________________|
+      |_____________________________________________________|
+"""
+os.system(f'echo "{text}" | lolcat -t')
+
+vvvv = input('''\033[1;34m
+
+            Enter Number\033[1;35m ==> √  \033[1;36m:    ''')
+if vvvv =='':
+   os.system('python Dragon-Tool.py')
+elif vvvv =="1":
+     os.system("python 1.py")
+elif vvvv =="2":
+     os.system("python 2.py")
+elif vvvv =="3":
+     os.system("python 3.py")
+elif vvvv =="4":
+     os.system("python 4.py")
+elif vvvv =="5":
+     os.system("python 5.py")
+elif vvvv =="6":
+     os.system("python 6.py")
+elif vvvv =="7":
+     os.system("bash 7.sh")
+elif vvvv =="8":
+     os.system("python 8.py")
+elif vvvv =="9":
+     os.system("python 9.py")
+elif vvvv =="10":
+     os.system("python 10.py")
+elif vvvv =="11":
+     os.system("python 11.py")
+elif vvvv =="12":
+     os.system("python 12.py")
+elif vvvv =="13":
+     os.system("python 13.py")
+elif vvvv =="13":
+     os.system("python 14.py")
+elif vvvv =="14":
+     os.system("cd")
+     os.system("sh termux-ngrok.sh")
+elif vvvv =="15":
+     os.system("python ngrok.py")
+elif vvvv =="16":
+     os.system("python ins.py")
+elif vvvv =="17":
+     os.system("bash setup.sh && tstyle")
